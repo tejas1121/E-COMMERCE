4d3fd98385d4363d26ddafebef7d556f6b0efda2
Tejas <tejasburkul967@gmail.com>
2026-02-28 11:08:47 +0530

    added tracking and orders pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@ import {Routes,Route} from 'react-router';
 import './App.css'
 import { HomePage } from './pages/HomePage'
 import { CheckoutPage } from './pages/CheckoutPage';
-
+import { OrdersPage } from './pages/OrdersPage';
+import { TrackingPage } from './pages/TrackingPage';
 function App() {
 
   return (
@@ -10,6 +11,8 @@ function App() {
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/checkout" element={<CheckoutPage />} />
+      <Route path="/orders" element={<OrdersPage />} />
+      <Route path="/tracking" element={<TrackingPage />} />
     </Routes>
     </>
   )
